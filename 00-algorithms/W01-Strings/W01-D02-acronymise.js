6bca0e7b51e92665c04fe9f381116b3449f45140
RanyaOUNI <136346306+RanyaOUNI@users.noreply.github.com>
2023-07-11 17:07:32 +0100

    test

---
+++ 00-algorithms/W01-Strings/W01-D02-acronymise.js
@@ -0,0 +1,54 @@
+/* 
+  Acronyms
+
+  Create a function that, given a string, returns the string’s acronym 
+  (first letter of each word capitalized). 
+
+  Do it with .split first if you need to, then try to do it without
+*/
+
+const str1 = "object oriented programming";
+// const expected1 = "OOP";
+
+// // The 4 pillars of OOP
+const str2 = "abstraction polymorphism inheritance encapsulation";
+// const expected2 = "APIE";
+
+const str3 = "software development life cycle";
+// const expected3 = "SDLC";
+
+// // Bonus: ignore extra spaces
+const str4 = "  global   information tracker    ";
+// const expected4 = "GIT";
+
+
+function acronymize(str) {
+  var result=""
+  for (var i=0;i<str.length-2;i++){
+    if(str[i] == ""&&str[i+1] != ""){
+      console.log(str[i+1].toUpperCase())
+      result += str[i+1].toUpperCase()
+    }
+  }
+}
+
+// Second Method:Split
+
+function acronymize(str){
+  var arr = str.split("")
+  var result = ""
+  console.log(arr);
+  for (var i=0;i<arr.length;i++)
+    if (arr[i] !=""){
+      result += arr[i][0].toUpperCase()
+    }
+    return result
+
+
+
+
+}
+
+
+
+
---
+++ 01-python-fundamentals/Core/for_loop_basic1.py
@@ -0,0 +1,36 @@
+#---------- INTEGERS FROM 0 TO 150 -----------
+
+#for i in range(0,151):
+    #print(i)
+
+#---------- MULTIPLES OF FIVE ----------------
+
+#for i in range(5,5001):
+    #if (i%5==0):
+        #print(i)
+
+#----------- COUNTING THE DOJO WAY -----------
+
+for i in range(1,101):
+    if (i%10==0):
+        print("Coding Dojo")
+    elif (i%5==0):
+        print("Coding")
+    else:
+        print(i)
+
+#------------ Whoa. That sucker's Huge ---------
+
+sum=0
+for i in range(0,500001):
+    if (i%2!=0):
+        sum=sum+i
+    print(sum)
+    
+#------------ Countdown by Fours ---------------
+
+for i in range(2018,0,-4):
+    print(i)
+
+#------------ Flexible Counter -----------------
+
