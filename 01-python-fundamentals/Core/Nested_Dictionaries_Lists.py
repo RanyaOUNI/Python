d114dddd0d9ca51ed24c25a0d806969324968854
RanyaOUNI <136346306+RanyaOUNI@users.noreply.github.com>
2023-07-13 13:46:38 +0100

    Core

--- 01-python-fundamentals/Core/Nested_Dictionaries_Lists.py
+++ 01-python-fundamentals/Core/Nested_Dictionaries_Lists.py
@@ -1,3 +1,4 @@
+
 # 1 ------ Update Values in Dictionaries and Lists
 
 x = [ [5,2,3], [10,8,9] ] 
@@ -46,10 +47,42 @@ IterateDictionary(students)
 
 # 3 ------ Get Values From a List of Dictionaries
 
-def IterateDictionary2(key,list):
-    for i in range(0,len(list)):
-        print(f"")
+students = [
+ {'first_name': 'Michael', 'last_name' : 'Jordan'},
+ {'first_name' : 'John', 'last_name' : 'Rosales'},
+ {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+ {'first_name' : 'KB', 'last_name' : 'Tonel'}
+ ]
 
+def IterateDictionary2(key_name,list):
+    
+     for student in list:
+        print(f"{student[key_name]}")
+        
+        
+IterateDictionary2('first_name',students)
 
 
 # 4 ------ Iterate Through a Dictionary with List Values
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+#printInfo(dojo)
+
+def printInfo(dict):
+    
+    for key,value in dict.items():
+        print(f"{len(value)} {key}")
+        for i in range(0,len(value)):
+            print(value[i])
+
+printInfo(dojo)
+
+
+
+
+
+
--- 01-python-fundamentals/Core/for_loop_basic1.py
+++ 01-python-fundamentals/Core/for_loop_basic1.py
@@ -1,13 +1,18 @@
+
 #---------- INTEGERS FROM 0 TO 150 -----------
 
 for i in range(0,151):
     print(i)
+
+print("************")
 
 #---------- MULTIPLES OF FIVE ----------------
 
 for i in range(5,5001):
     if (i%5==0):
         print(i)
+
+print("************")
 
 #----------- COUNTING THE DOJO WAY -----------
 
@@ -19,18 +24,28 @@ for i in range(1,101):
     else:
         print(i)
 
-#------------ Whoa. That sucker's Huge ---------
+print("************")
 
+#------------ Whoa. That sucker's Huge ---------
 sum=0
 for i in range(0,500001):
     if (i%2!=0):
-        sum=sum+i
-    print(sum)
-    
+        sum+=i
+print(sum)
+
+print("************")
+
+
 #------------ Countdown by Fours ---------------
 
 for i in range(2018,0,-4):
     print(i)
 
-#------------ Flexible Counter -----------------
+print("************")
 
+#------------ Flexible Counter -----------------
+def Flexible(lowNum,highNum,mult):
+    for i in range(lowNum,highNum+1):
+        if (i%mult==0):
+            print(i)
+Flexible(2,9,3)
