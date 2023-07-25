0eba8fdc1cab4c44839741e52b8d7ec5df26f050
RanyaOUNI <136346306+RanyaOUNI@users.noreply.github.com>
2023-07-25 14:02:59 +0100

    UserCR

--- 05-Flask&MySQL/Practise/User-CR/server.py
+++ 05-Flask&MySQL/Practise/User-CR/server.py
@@ -1,16 +1,30 @@
 from flask import Flask,render_template,redirect,request
-
+from user_model import User
 app = Flask(__name__)
 
 
 @app.route('/')
 def dashboard():
-    user = User.get_all()
-    return render_template('Read.html',user=user)
+    all_users = User.get_all()
+    return render_template('Read.html',users=all_users)
 
+@app.route('/users/new')
+def new_user():
+     return render_template('Create.html')
+
+@app.route('/users/Create',methods=['POST'])
+def create_user():
+    print(request.form)
+    data_dict = {
+        'first_name':request.form['first_name'],
+        'last_name':request.form['last_name'],
+        'email':request.form['email']    
+        }
+    User.create_user(data_dict)
+    return redirect('/')
 
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+    app.run(debug=True,port=5000)
--- 05-Flask&MySQL/Practise/User-CR/user_model.py
+++ 05-Flask&MySQL/Practise/User-CR/user_model.py
@@ -1,6 +1,7 @@
 
 from pymysqlconnection import connectToMySQL
 class User: 
+
     def __init__(self,data_dict):
         self.id = data_dict['id']
         self.first_name = data_dict['first_name']
@@ -9,10 +10,24 @@ class User:
         self.created_at = data_dict['created_at']
         self.updated_at = data_dict['updated_at']
 
-@classmethod
-def get_all(cls):
-    query = "SELECT * FROM users;"
-    results = connectToMySQL("users_cr").query_db(query)
-    # print(results)
-    
-    return all_users
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL("users_cr").query_db(query)
+        all_users = []
+        for row in results:
+            user = cls(row)
+            all_users.append(user)
+        print(all_users)
+        return all_users
+
+    @classmethod
+    def create_user(cls,data_dict):
+        query = """
+                INSERT INTO users (first_name,last_name,email)
+                VALUES (%(first_name)s,%(last_name)s,%(email)s)
+        """
+        result = connectToMySQL("users_cr").query_db(query,data_dict)
+        print(result)
+
+        return None
